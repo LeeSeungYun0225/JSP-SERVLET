cb43907ac4c5e852d6d7ffd0041531682e74cab0
Seung Yun LEE <soul3914@naver.com>
2022-06-29 01:40:05 +0900

    jsp/servlet 4th

--- src/main/java/com/newlecture/web/NoticeReg.java
+++ src/main/java/com/newlecture/web/NoticeReg.java
@@ -23,8 +23,20 @@ public class NoticeReg extends HttpServlet {
 		
 		
 		response.setCharacterEncoding("UTF-8");
+		
+		
+		//request.setCharacterEncoding("UTF-8");
+		//한글 입력시 깨지는 문제 발생 
+				// 한글은 기본적으로 글자당 2바이트, 영어는 1바이트,
+				//post로 전송시에 ISO-8859-1형태로 인코딩방식을 이용해 
+				//1바이트를 1문자로 인식함 
+			//  request를 입력받기 전에 UTF-8이라고 알려주면 된다 .
 		//UTF-8형식으로 인코딩 
 		// 브라우저에게 UTF-8 형식으로 보낸다고 알려주어야 한다.
+		
+		
+		
+		//UTF-8로 설정 안하고 필터를 사용해보기 
 		
 		response.setContentType("text/html; charset=UTF-8");
 		//파일의 형식 : html , 텍스트 형식 : UTF8로 보낸다고 알려주는 문장 
@@ -37,6 +49,7 @@ public class NoticeReg extends HttpServlet {
 		String content = request.getParameter("content");
 		//쿼리스트링이 너무 길어지는 문제 발생 
 		//html에서 method="post" 사용하면 해결 
+		
 		
 		
 		out.println(title);
---
+++ src/main/java/com/newlecture/web/filter/CharacterEncodingFilter.java
@@ -0,0 +1,44 @@
+package com.newlecture.web.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+
+//필터가 was로부터 들어오는 요구사항을 servlet에 도착하기 전
+// 먼저 확인하고 전처리가능 
+// servlet에서 응답시에도 필터를 거쳐서 나가므로 후처리도 가능 
+@WebFilter("/*")
+public class CharacterEncodingFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request,
+			ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		request.setCharacterEncoding("UTF-8");
+		chain.doFilter(request, response);
+		
+		System.out.println("hello filter");
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
