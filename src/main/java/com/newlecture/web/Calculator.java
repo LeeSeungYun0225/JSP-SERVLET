c455173d8d48c27e4674fef40cc57ffd3de1273b
Seung Yun LEE <soul3914@naver.com>
2022-06-29 02:55:38 +0900

    jsp/servlet 5th

---
+++ src/main/java/com/newlecture/web/Calculator.java
@@ -0,0 +1,24 @@
+package com.newlecture.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/calculator")
+public class Calculator extends HttpServlet{
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException,ServletException
+	{
+		PrintWriter out = response.getWriter();
+		
+		int first = Integer.parseInt(request.getParameter("first"));
+		int second = Integer.parseInt(request.getParameter("second"));
+		
+		out.println(first+second);
+	}
+}
