73caa23fadb0048545b481d75f29b0a6a2ff8e29
Seung Yun LEE <soul3914@naver.com>
2022-06-29 21:15:09 +0900

    jsp/servlet 8th

---
+++ src/main/java/com/newlecture/web/Calculator2.java
@@ -0,0 +1,70 @@
+package com.newlecture.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/calculator2")
+public class Calculator2 extends HttpServlet{
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException,ServletException
+	{
+		PrintWriter out = response.getWriter();
+		
+		int v = Integer.parseInt(request.getParameter("v"));
+		String in = request.getParameter("operator");
+		
+		
+		// application 컨텍스트에 value의 이름으로 v를 저장
+		// opper의 이름으로 in을 저장 
+		
+		ServletContext application = request.getServletContext();
+		// 어플리케이션 전역에서 사용할 수 있다
+		
+		
+		HttpSession session = request.getSession();
+		// 세션 범주 내에서 사용할 수 있다.
+		// 세션 = 현재 접속한 사용자 연결 >> 접속자마다 별도로 관리 
+		// 프로세스별로 세션을 관리하기때문에 같은 브라우저는 하나의 프로세스에 다중 스레드이므로 같은 세션 취급
+		// 다른 브라우저는 서로 프로세스가 다르기 때문에 다른 세션으로 인식
+		
+		
+		
+		if(in.equals("=")) //연산 버튼 누르면 
+		{
+			int x= (int) session.getAttribute("value");
+			//int x= (int) application.getAttribute("value");
+			int y = v;
+			String op = (String) session.getAttribute("opper");
+			//String op = (String) application.getAttribute("opper");
+			int result =0;
+			if(op.equals("+"))
+			{
+				result = x+y;
+			}
+			else
+			{
+				result = x-y;
+			}
+			out.println(result);
+		}
+		else // 덧셈이나 뺄셈일때 
+		{ // 내용을 저장 
+			
+			//application.setAttribute("value", v);
+			//application.setAttribute("opper", in);
+			session.setAttribute("value", v);
+			session.setAttribute("opper", in);
+		}	
+		
+		
+
+	}
+}
