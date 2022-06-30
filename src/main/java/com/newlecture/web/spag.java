345d71cde833299e727266d267cea5918b7d6290
Seung Yun LEE <soul3914@naver.com>
2022-07-01 00:59:11 +0900

    jsp/servlet MVC

---
+++ src/main/java/com/newlecture/web/spag.java
@@ -0,0 +1,55 @@
+package com.newlecture.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/spa")
+public class spag extends HttpServlet{
+
+	//컨트롤러 역할 // 실행은 컨트롤러에서..
+	protected void doGet(HttpServletRequest request,HttpServletResponse response)
+	{
+		int num = 0;
+		String num_ = request.getParameter("num");
+		if(num_ != null && !num_.equals(""))
+		{
+			num = Integer.parseInt(num_);
+		}
+
+
+		String result; // >> 모델 역할 
+
+		if(num%2 !=0)
+		{
+			result = "홀수";
+		}
+		else
+		{
+			result = "짝수";
+		}
+
+		
+		///포워딩 : 현재 작업한 내용을 이어가면서 연결  /// 
+		// 리디렉트 : 현재 작업한 내용과 무관하게 연결
+		
+		request.setAttribute("result", result);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("spa.jsp");
+		
+		try {
+			dispatcher.forward(request,response); // 포워드로 공유하며 사용할 수 있는 저장소 request.
+		} catch (ServletException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+}
--- src/main/webapp/spa.jsp
+++ src/main/webapp/spa.jsp
@@ -2,12 +2,6 @@
     pageEncoding="UTF-8"%>
     
     <% 
-int num = 0;
-String num_ = request.getParameter("num");
-if(num_ != null && !num_.equals(""))
-{
-	num = Integer.parseInt(num_);
-}
 
 %>
 <!DOCTYPE html>
@@ -17,16 +11,6 @@ if(num_ != null && !num_.equals(""))
 <title>Insert title here</title>
 </head>
 <body>
-
-
-<% if(num%2 != 0) 
-{%>
-	홀수입니다.
-<% }
-else 
-{ %>
-	짝수입니다.
-<%} %>
-
+	<%=request.getAttribute("result") %>입니다.
 </body>
 </html>
